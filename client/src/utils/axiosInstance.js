802d00d409f94a2194df5090e73041d421d3861a
adarshsharma584 <adarshsharma0862@gmail.com>
2025-12-16 16:57:54 +0530

    C:/Program Files/Git/fix - axiosInstance bug

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Provide a sensible fallback for local development when VITE_API_URL isn't set.
-const baseURL = import.meta.env.VITE_API_URL";
+const baseURL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL,
